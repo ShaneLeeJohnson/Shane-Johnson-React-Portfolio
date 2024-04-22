69e6a99fa844d4ebd26da2c7c8bd0388b23e0e5b
shanejohnson <shane.john316@gmail.com>
2024-04-22 10:51:56 -0500

    Deleted the standard vite images and modified the page layout to use the components for Header, Content, and Footer that I created

---
+++ Shane-Johnson-React-Portfolio/src/Footer.jsx
@@ -0,0 +1,10 @@
+function Footer() {
+    return (
+        <footer className="footer">
+            <p>Contact: shane.johnson@gmail.com</p>
+            <p>&copy; 2024 Shane Johnson</p>
+        </footer>
+    );
+}
+
+export default Footer;
