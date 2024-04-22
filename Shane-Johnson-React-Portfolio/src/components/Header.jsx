63a700759939822d0e88f25e715caa0a6516ae2b
shanejohnson <shane.john316@gmail.com>
2024-04-22 16:00:40 -0500

    created pages for About Me, Portfolio, Contact and Resume, and added ReactRouter code to allow the user to navigate between the pages without having the page reload

--- Shane-Johnson-React-Portfolio/src/components/Header.jsx
+++ Shane-Johnson-React-Portfolio/src/components/Header.jsx
@@ -1,15 +1,26 @@
 import "./Header.css"
+import { Link, useLocation } from 'react-router-dom';
 
 function Header() {
+    const location = useLocation();
+
     return (
         <header className="header">
             <h1>Shane Johnson</h1>
             <nav>
                 <ul className="nav-links">
-                    <li><a href="#about">About Me</a></li>
-                    <li><a href="#portfolio">Portfolio</a></li>
-                    <li><a href="#contact">Contact</a></li>
-                    <li><a href="#resume">Resume</a></li>
+                    <li className={location.pathname === "/" ? "active" : ""}>
+                        <Link to="/">About Me</Link>
+                    </li>
+                    <li className={location.pathname === "/portfolio" ? "active" : ""}>
+                        <Link to="/portfolio">Portfolio</Link>
+                    </li>
+                    <li className={location.pathname === "/contact" ? "active" : ""}>
+                        <Link to="/contact">Contact</Link>
+                    </li>
+                    <li className={location.pathname === "/resume" ? "active" : ""}>
+                        <Link to="/resume">Resume</Link>
+                    </li>
                 </ul>
             </nav>
         </header>
--- Shane-Johnson-React-Portfolio/src/components/main.jsx
+++ Shane-Johnson-React-Portfolio/src/components/main.jsx
@@ -1,10 +1,40 @@
-import React from 'react'
-import ReactDOM from 'react-dom/client'
-import App from './App.jsx'
-import './index.css'
+import React from 'react';
+import ReactDOM from 'react-dom/client';
+import { createBrowserRouter, RouterProvider } from 'react-router-dom';
+import App from './App.jsx';
+import About from '../pages/About.jsx';
+import Error from '../pages/Error.jsx';
+import Portfolio from '../pages/Portfolio.jsx';
+import Contact from '../pages/Contact.jsx';
+import Resume from '../pages/Resume.jsx';
+import './index.css';
+
+const router = createBrowserRouter([
+  {
+    path: '/',
+    element: <App />,
+    errorElement: <Error />,
+    children: [
+      {
+        index: true,
+        element: <About />,
+      },
+      {
+        path: 'Portfolio',
+        element: <Portfolio />,
+      },
+      {
+        path: 'Contact',
+        element: <Contact />,
+      },
+      {
+        path: 'Resume',
+        element: <Resume />
+      }
+    ]
+  }
+])
 
 ReactDOM.createRoot(document.getElementById('root')).render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>,
+  <RouterProvider router={router} />
 )
