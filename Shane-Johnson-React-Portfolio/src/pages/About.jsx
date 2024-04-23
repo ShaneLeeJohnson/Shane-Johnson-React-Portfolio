81fdfce28cacab004c352cc18ba34bcb09307ca2
shanejohnson <shane.john316@gmail.com>
2024-04-23 13:18:42 -0500

    modified the about me page by adding a profile picture and some bullet points highlighting my strengths as a developer. I also moved the app.jsx, app.css, main.jsx and index.css files outside of the components directory

--- Shane-Johnson-React-Portfolio/src/pages/About.jsx
+++ Shane-Johnson-React-Portfolio/src/pages/About.jsx
@@ -1,5 +1,19 @@
+import './About.css'
+
 export default function About() {
     return (
-        <h1>About Page</h1>
+        <section id="about" className="about">
+            <h2>About Me</h2>
+            <img src="/assets/ProfilePic.jpg" alt="Shane Johnson" className="profile-pic" />
+            <p>
+                <ul>
+                    <li>Enrolled in the Full Stack Coding Bootcamp at UTSA, graduating with a certificate on May 13th 2024.</li>
+                    <li>Developed strong foundations in front-end development languages: HTML, CSS, and JavaScript.</li>
+                    <li>Eager learner with a passion for continuous skill development. Possess a basic understanding of Python.</li>
+                    <li>Seeking a front-end web developer role to gain practical experience while further honing back-end development skills.</li>
+                    <li>Analytical and results-oriented with a strong focus on problem-solving. Confident in my ability to approach challenges with a proactive mindset, research solutions effectively, and implement them successfully.</li>
+                </ul>
+            </p>
+        </section>
     )
 }
